03b229d12941ae329afb5e7f56b56d0e0abe10bc
Joao Augusto Perin de Souza <joaoaugustoperin@gmail.com>
2022-03-02 16:09:26 -0300

    :white_check_mark: Fix test file

--- test/solidity_watcher_test.exs
+++ test/solidity_watcher_test.exs
@@ -2,16 +2,17 @@ defmodule SolidityWatcherTest do
   use ExUnit.Case
   doctest SolidityWatcher
   @otp_app Application.get_application(__MODULE__)
+  import Config
 
   test "installs the solc compiler" do
     version = Application.get_env(@otp_app, :version, "latest")
 
     Solidity.Watcher.install()
 
-    assert "#{Mix.Project.build_path()}/solidity-#{version}"
+    assert File.exists?("#{Mix.Project.build_path()}/../solidity-#{version}")
 
     unless version == "latest" do
-      assert System.shell("#{Mix.Project.build_path()}/solidity-#{version}/solc --version")
+      assert System.shell("#{Mix.Project.build_path()}/../solidity-#{version} --version")
              |> elem(0)
              |> Kernel.=~(version)
     end
