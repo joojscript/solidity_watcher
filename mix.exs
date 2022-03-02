fb2ef4845836cf78dd58ba97df70e4755b81a520
Joao Augusto Perin de Souza <joaoaugustoperin@gmail.com>
2022-03-02 19:38:09 -0300

    :bookmark: Fix version to 0.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SolidityWatcher.MixProject do
   def project do
     [
       app: :solidity_watcher,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: ">= 1.13.3",
       start_permanent: Mix.env() == :prod,
       deps: deps()
