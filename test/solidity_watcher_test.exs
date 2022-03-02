30921b9b5ed7d0a038c3919aa6d13b9e31df7b00
Joao Augusto Perin de Souza <joaoaugustoperin@gmail.com>
2022-03-02 15:27:16 -0300

    :test_tube: Add watcher primary test

--- test/solidity_watcher_test.exs
+++ test/solidity_watcher_test.exs
@@ -1,8 +1,19 @@
 defmodule SolidityWatcherTest do
   use ExUnit.Case
   doctest SolidityWatcher
+  @otp_app Application.get_application(__MODULE__)
 
-  test "greets the world" do
-    assert SolidityWatcher.hello() == :world
+  test "installs the solc compiler" do
+    version = Application.get_env(@otp_app, :version, "latest")
+
+    Solidity.Watcher.install()
+
+    assert "#{Mix.Project.build_path()}/solidity-#{version}"
+
+    unless version == "latest" do
+      assert System.shell("#{Mix.Project.build_path()}/solidity-#{version}/solc --version")
+             |> elem(0)
+             |> Kernel.=~(version)
+    end
   end
 end
